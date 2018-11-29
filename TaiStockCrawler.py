7b995cab69dcd8707538dbc20c3f498c63bdb6b8
lineagech <lineagech@gmail.com>
2018-11-29 17:42:00 +0800

    use pandas to get web data

--- TaiStockCrawler.py
+++ TaiStockCrawler.py
@@ -1,19 +1,70 @@
 import certifi
 import urllib3
 import urllib.request
+from lxml import etree
 
 
 url = "https://www.cnyes.com/twstock/sec_quote.aspx?code=FTW50"
+url = "https://www.cnyes.com/twfutures/quote_future.aspx"
+url = "http://www.stockq.org/index/TWSE.php"
+url = "http://www.twse.com.tw/zh/page/trading/indices/MI_5MINS_HIST.html"
 #http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 #req = http.request("GET", url)
 #html = urllib3.urlopen(req)
 
 req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as f:
-    print(f.read().decode('utf-8'))
+req.add_header("User-Agent",
+"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36");
+
+form_data = {
+    "yy":"2018",
+    "mm":"11"
+}
+
+html = None
+form_data = urllib.parse.urlencode(form_data).encode("utf-8");
+with urllib.request.urlopen(req, data=form_data) as f:
+    html = f.read()
+
+page = etree.HTML(html)
+for str in page.xpath(u"//tr[@class='row2']/descendant::text()"):
+    print(str)
 
 
+##############################################
 
+import pandas as pd
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+def get_companies(ex="NASDAQ"):
+    url = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange={}&render=download"
+    url = url.format(ex)
+    return pd.read_csv(url)
+
+start_date = datetime(2016,1,1)
+end_date = datetime.today()
+prices = web.DataReader('AAPL', 'yahoo', start_date, end_date)
+
+print(prices.shape)
+print("-----------------------")
+print(prices.describe())
+print("-----------------------")
+print(prices.head(3))
+print("-----------------------")
+print(prices.tail(3))
+print("-----------------------")
+print(prices.columns)
+print("-----------------------")
+print(prices.index)
+print("-----------------------")
+print(prices.info)
+print(prices.loc['2015-12-31'][1])
+
+
+prices[['Close','Volume']].plot(subplots = True, figsize = (10, 8))
+plt.legend(loc='best')
 
 
 
