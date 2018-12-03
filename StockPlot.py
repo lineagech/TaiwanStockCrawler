b70e444bcda36cfc8685eee112770943196e0145
lineagech <lineagech@gmail.com>
2018-12-03 12:46:01 +0800

    update code for stock plot

--- StockPlot.py
+++ StockPlot.py
@@ -6,6 +6,8 @@ import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 from matplotlib import gridspec
+from matplotlib import collections
+from matplotlib.colors import colorConverter
 
 def get_dates(startdate, enddate):
     dates = [datetime.date(m//12,m%12+1,1) for m in range(startdate.year*12+startdate.month-1, enddate.year*12+enddate.month)]
@@ -22,9 +24,57 @@ def get_xticks_name(list_dates, date_indexes):
         name_lists.append(list_dates[index].strftime("%b'%y"))
     return np.array(name_lists)
 
+def draw_values(ax, opens, highs, lows, closes, width=4, up_color='r', down_color='g', alpha=0.75):
+    '''K-bars'''
+    delta = width / 2.
+    barVerts = [((i - delta, open),
+                 (i - delta, close),
+                 (i + delta, close),
+                 (i + delta, open))
+                for i, open, close in zip(range(len(opens)), opens, closes)]
+
+    downSegments = [((i, low), (i, min(open, close)))
+                    for i, low, high, open, close in zip(range(len(lows)), lows, highs, opens, closes)]
+
+    upSegments = [((i, max(open, close)), (i, high))
+                  for i, low, high, open, close in zip(range(len(lows)), lows, highs, opens, closes)]
+    segments = downSegments + upSegments
+
+    r, g, b = colorConverter.to_rgb(up_color)
+    up_color = r, g, b, alpha
+    r, g, b = colorConverter.to_rgb(down_color)
+    down_color = r, g, b, alpha
+
+    color_dict = {True: up_color, False: down_color,}
+    colors = [color_dict[open < close] for open, close in zip(opens, closes)]
+
+    line_collections = collections.LineCollection( segments,
+                                                   linewidths=(0.5,),
+                                                   colors=((0,0,0,1),),
+                                                   antialiaseds=(0,)
+                                                 )
+    bar_collections = collections.PolyCollection( barVerts,
+                                      facecolors=colors,
+                                      edgecolors=((0, 0, 0, 1),),
+                                      antialiaseds=(0,),
+                                      linewidths=(0.5,),
+                                    )
+    min_x, max_x = 0, len(opens)
+    min_y = min([low for low in lows])
+    max_y = max([high for high in highs])
+
+    corners = (min_x, min_y), (max_x, max_y)
+    ax.update_datalim(corners)
+    ax.autoscale_view()
+
+    ax.add_collection(line_collections)
+    ax.add_collection(bar_collections)
+
+    return line_collections, bar_collections
+
 def draw(df, title="", up_color='r', down_color='g'):
-    startdate = df.Date[0]
-    enddate = df.Date[-1]
+    startdate = datetime.date(df.index[0].year, df.index[0].month, df.index[0].day)
+    enddate = datetime.date(df.index[-1].year, df.index[-1].month, df.index[-1].day)
 
     ''' Show prices '''
     def prices_format(x,y):
@@ -67,6 +117,8 @@ def draw(df, title="", up_color='r', down_color='g'):
     ax0 = plt.subplot(gd[0,:])
     ax1 = plt.subplot(gd[1,:], sharex=ax0)
 
+    draw_values(ax0, df.Open, df.High, df.Low, df.Close, width=1)
+
     dates_array = get_dates(startdate, enddate)
     xticks_date_indexes = get_xticks_date_index(df.index, dates_array)
     xticks_date_name = get_xticks_name(df.index, xticks_date_indexes) ## str array
@@ -78,7 +130,7 @@ def draw(df, title="", up_color='r', down_color='g'):
     ax0.set_xticklabels(xticks_date_name)
     ax1.set_xticks(xticks_date_indexes)
     ax1.set_xticklabels(xticks_date_name)
-    ax1.tick_params(axis='x',direction='out',length=5)
+    ax1.tick_params(axis='x',direction='out',length=5,labelsize=8)
 
     '''Set up format coord when cursor points at a specific point'''
     ax0.format_coord = prices_format
@@ -110,6 +162,8 @@ def draw(df, title="", up_color='r', down_color='g'):
 
     #ax0.autoscale_view()
     #ax1.autoscale_view()
+
+    plt.show()
 
 def main():
     startdate = datetime.date(2016,1,1)
